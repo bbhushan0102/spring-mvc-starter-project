67fe98c8ad3921aa068748461cedc7c23c4e5b7b
Bharat Bhushan <bharat@Bharats-MacBook-Pro.local>
2025-04-12 15:49:45 +0100

    display the table data in UI using home controller and home.html

--- src/main/java/org/studyeasy/SpringStarter/Controller/HomeController.java
+++ src/main/java/org/studyeasy/SpringStarter/Controller/HomeController.java
@@ -1,12 +1,21 @@
 package org.studyeasy.SpringStarter.Controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.studyeasy.SpringStarter.models.Post;
+import org.studyeasy.SpringStarter.services.PostService;
 
 @Controller
 public class HomeController {
+    @Autowired
+    PostService postService;
     @GetMapping("/")
     public String home (Model model) {
+        List <Post> posts = postService.getAll();
+        model.addAttribute("posts", posts);
         return "home";
     }
     @GetMapping("/register")
