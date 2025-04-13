c9e60cf334a27511972f01bb16a281a2da2d56f1
Bharat Bhushan <bharat@Bharats-MacBook-Pro.local>
2025-04-13 15:28:13 +0100

    web security added and service is back up and working

--- src/main/java/org/studyeasy/SpringStarter/Controller/AccountController.java
+++ src/main/java/org/studyeasy/SpringStarter/Controller/AccountController.java
@@ -5,11 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.studyeasy.SpringStarter.models.Account;
 import org.studyeasy.SpringStarter.services.AccountService;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 
--- src/main/java/org/studyeasy/SpringStarter/config/WebSecurityConfig.java
+++ src/main/java/org/studyeasy/SpringStarter/config/WebSecurityConfig.java
@@ -1,12 +1,14 @@
 package org.studyeasy.SpringStarter.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 
+@Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebSecurityConfig {
@@ -22,6 +24,7 @@ public class WebSecurityConfig {
     };
 
     
+    @SuppressWarnings("removal")
     @Bean
 public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
